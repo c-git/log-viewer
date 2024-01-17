3240fe11f1ad588d3cc4176ba60460a12161382d
One <43485962+c-git@users.noreply.github.com>
2024-01-16 20:57:34 -0500

    Show a Table

--- src/app.rs
+++ src/app.rs
@@ -1,22 +1,11 @@
-/// We derive Deserialize/Serialize so we can persist app state on shutdown.
-#[derive(serde::Deserialize, serde::Serialize)]
+// TODO 3: Add search
+// TODO 3: Add filter by and let user pick like ID or date or something like that
+//
+// We derive Deserialize/Serialize so we can persist app state on shutdown.
+#[derive(serde::Deserialize, serde::Serialize, Debug, Default)]
 #[serde(default)] // if we add new fields, give them default values when deserializing old state
 pub struct LogViewerApp {
-    // Example stuff:
-    label: String,
-
-    #[serde(skip)] // This how you opt-out of serialization of a field
-    value: f32,
-}
-
-impl Default for LogViewerApp {
-    fn default() -> Self {
-        Self {
-            // Example stuff:
-            label: "Hello World!".to_owned(),
-            value: 2.7,
-        }
-    }
+    selected_row: Option<usize>,
 }
 
 impl LogViewerApp {
@@ -32,6 +21,83 @@ impl LogViewerApp {
         }
 
         Default::default()
+    }
+
+    fn show_log_lines(&mut self, ui: &mut egui::Ui) {
+        use egui_extras::{Column, TableBuilder};
+
+        let text_height = egui::TextStyle::Body
+            .resolve(ui.style())
+            .size
+            .max(ui.spacing().interact_size.y);
+
+        let mut table = TableBuilder::new(ui)
+            .striped(true)
+            .resizable(true)
+            .cell_layout(egui::Layout::left_to_right(egui::Align::LEFT))
+            .column(Column::auto())
+            .column(Column::auto())
+            .column(Column::auto())
+            .column(Column::remainder())
+            .min_scrolled_height(0.0);
+
+        // Make table clickable
+        table = table.sense(egui::Sense::click());
+        let data: Vec<i32> = (1..=5).collect();
+        let num_rows = data.len();
+
+        table
+            .header(20.0, |mut header| {
+                header.col(|ui| {
+                    ui.strong("Time");
+                });
+                header.col(|ui| {
+                    ui.strong("request_id");
+                });
+                header.col(|ui| {
+                    ui.strong("otel.name");
+                });
+                header.col(|ui| {
+                    ui.strong("msg");
+                });
+            })
+            .body(|body| {
+                body.rows(text_height, num_rows, |mut row| {
+                    let row_index = row.index();
+                    if let Some(selected_row) = self.selected_row {
+                        row.set_selected(selected_row == row_index);
+                    }
+                    row.col(|ui| {
+                        ui.label(format!("{}", data[row_index]));
+                    });
+                    row.col(|ui| {
+                        ui.label(format!("{}", data[row_index]));
+                    });
+                    row.col(|ui| {
+                        ui.label(format!("{}", data[row_index]));
+                    });
+                    row.col(|ui| {
+                        ui.label(format!("{}", data[row_index]));
+                    });
+
+                    self.toggle_row_selection(row_index, &row.response());
+                });
+            });
+    }
+
+    fn show_log_details(&self, ui: &mut egui::Ui) {
+        // TODO 1: Log Details
+        ui.label("Hi");
+    }
+
+    fn toggle_row_selection(&mut self, row_index: usize, row_response: &egui::Response) {
+        if row_response.clicked() {
+            if Some(row_index) == self.selected_row {
+                self.selected_row = None;
+            } else {
+                self.selected_row = Some(row_index);
+            }
+        }
     }
 }
 
@@ -68,6 +134,20 @@ impl eframe::App for LogViewerApp {
         egui::CentralPanel::default().show(ctx, |ui| {
             // The central panel the region left after adding TopPanel's and SidePanel's
             ui.heading("Log Viewer");
+            use egui_extras::{Size, StripBuilder};
+            StripBuilder::new(ui)
+                .size(Size::remainder().at_least(100.0)) // for the table
+                .size(Size::exact(100.0)) // for the details
+                .vertical(|mut strip| {
+                    strip.cell(|ui| {
+                        egui::ScrollArea::horizontal().show(ui, |ui| {
+                            self.show_log_lines(ui);
+                        });
+                    });
+                    strip.cell(|ui| {
+                        self.show_log_details(ui);
+                    });
+                });
         });
     }
 }
