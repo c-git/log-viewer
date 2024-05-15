d597ef514666a616a8acafa6a8c73137a9c1a138
One <43485962+c-git@users.noreply.github.com>
2024-05-14 22:03:14 -0400

    chore: document issues found during use

--- src/lib.rs
+++ src/lib.rs
@@ -2,3 +2,7 @@
 
 mod app;
 pub use app::LogViewerApp;
+
+// TODO 3: Fix bug where multiline messages bleed into the lines below them
+// TODO 4: Add a open most recent log button
+// TODO 4: Add a reload log button
