ec1d1433f505a9f50b3504c35c5c1e99fbd6cd52
One <43485962+c-git@users.noreply.github.com>
2024-07-24 01:05:13 -0400

    chore: save link to code found

--- src/lib.rs
+++ src/lib.rs
@@ -8,5 +8,5 @@ pub use app::LogViewerApp;
 // TODO 3: Add checkbox to filter by current request id
 // TODO 3: Add filter by and let user pick like ID or date or something like that
 // TODO 3: Add search
-// TODO 3: Add support for navigating using arrow keys
+// TODO 3: Add support for navigating using arrow keys Implement hot keys https://github.com/c-git/egui/blob/34db001db14940c948eb03d3fe87f2af2c45daba/crates/egui_demo_lib/src/demo/demo_app_windows.rs#L323
 // TODO 5: Support auto reload (look into watching for changes)
